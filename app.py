89bae7ae3b0c1cd65ee57b52f16219e1f18ecf9f
PA <45755573+EquinetPaul@users.noreply.github.com>
2023-11-29 22:34:41 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,57 +1,77 @@
-from flask import Flask, render_template, request
-import pandas as pd
-import numpy as np
-from datetime import timedelta
+"""
+CompareWhatt
+"""
 import json
+from datetime import timedelta
+import pandas as pd
+from flask import Flask, render_template, request
 import plotly
 import plotly.graph_objs as go
 
-# https://www.electrodepot.fr/refrigerateur-2-portes-high-one-2d-206-f-w742c-206l-blanc.html
-# https://www.electrodepot.fr/refrigerateur-combine-valberg-cs-311-d-x625c.html
-# https://www.electrodepot.fr/refrigerateur-combine-daewoo-ckm0379cana0.html
-
 app = Flask(__name__)
 
-def projection(df : pd.DataFrame, electricity_price):
+def projection(df : pd.DataFrame, electricity_price: float, theorical_evolution : float):
+    """
+    Projette les coûts totaux pour chaque produit dans un DataFrame sur une période de 50 ans.
+
+    Paramètres:
+    df (pd.DataFrame): DataFrame contenant les données des produits
+    electricity_price (float): Le prix de l'électricité par kWh.
+    theorical_evolution (float): Evolution théorique du prix du kWh/année
+
+    Retourne:
+    pd.DataFrame: Un DataFrame avec les coûts totaux projetés pour chaque produit et chaque jour sur une période de 50 ans.
+    """
     start_date = pd.to_datetime('today')
     end_date = start_date + timedelta(days=50*365)
     dates = pd.date_range(start=start_date, end=end_date, freq='D')
-    
     df['Consommation (kWh/annum)'] = df['Consommation (kWh/annum)'].astype(float)
-
     df['Daily_Consumption'] = df['Consommation (kWh/annum)'] / 365
-    
     rows = []
     for _, row in df.iterrows():
         for date in dates:
+            years_passed = (date.year - start_date.year)
+            adjusted_electricity_price = float(electricity_price) * ((1 + float(theorical_evolution)) ** years_passed)
             days_passed = (date - start_date).days
-            total_price = float(row['Prix de vente']) + float(row['Daily_Consumption']) * float(days_passed) * float(electricity_price)
-            rows.append({'Total_Price': total_price, 'Date': date, 'Product': int(row['id'])})
-            
+            total_price = float(row['Prix de vente']) + row['Daily_Consumption'] * days_passed * adjusted_electricity_price
+            rows.append({'Total_Price': total_price, 'Date': date, 'Product': row['id']})    
     final_df = pd.DataFrame(rows)
     final_df['Date'] = pd.to_datetime(final_df['Date'])
     final_df['Date'] = final_df['Date'].dt.strftime('%d/%m/%Y')
-    
     return final_df
 
 def transform_projection(df: pd.DataFrame):
+    """
+    Transforme les données projetées en une structure JSON adaptée pour la visualisation.
+
+    Paramètres:
+    df (pd.DataFrame): DataFrame contenant les données projetées
+
+    Retourne:
+    str: Une chaîne JSON représentant les données transformées, adaptée pour la visualisation.
+    """
     df['Date'] = pd.to_datetime(df['Date'], format='%d/%m/%Y').astype(str)
     data = []
-
     for product_id, group in df.groupby('Product'):
         product_data = {
             'label': f'Produit {product_id}',
             'data': group[['Date', 'Total_Price']].to_dict('records'),
         }
         data.append(product_data)
-
     data_json = json.dumps(data)
-
     return data_json
 
 def transform_projection_plotly(df: pd.DataFrame):
-    data = []
+    """
+    Prépare les données pour la visualisation avec Plotly, en formatant les données projetées en traces Plotly.
 
+    Paramètres:
+    df (pd.DataFrame): DataFrame contenant les données projetées
+
+    Retourne:
+    str: Une chaîne JSON représentant le graphique Plotly, adaptée pour la visualisation interactive.
+    """
+    data = []
     for product_id, group in df.groupby('Product'):
         trace = go.Scatter(
             x=group['Date'],
@@ -62,7 +82,6 @@ def transform_projection_plotly(df: pd.DataFrame):
             text=group.apply(lambda row: f'Date: {row.Date}, Prix: {row.Total_Price}', axis=1)
         )
         data.append(trace)
-
     layout = dict(
         title='Comparaison des Prix de Produits',
         xaxis=dict(
@@ -70,14 +89,15 @@ def transform_projection_plotly(df: pd.DataFrame):
         ),
         yaxis=dict(title='Prix Total'),
     )
-
     fig = dict(data=data, layout=layout)
     graph_json = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
     return graph_json
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    """
+    Fonction principale et gestionnaire des routes HTTP
+    """
     products_data = []
     if request.method == 'POST':
         electricity_price = request.form.get("electricity_price")
@@ -94,14 +114,11 @@ def index():
 
 
         products_df = pd.DataFrame(products_data)
-        projection_df = projection(products_df, electricity_price)
+        projection_df = projection(products_df, electricity_price, theorical_evolution)
         # products_df.to_csv("save.csv")
         # projection_to_template = transform_projection(projection_df)
         projection_to_template = transform_projection_plotly(projection_df)
-
-
         return render_template('index.html', products=products_data, electricity_price=electricity_price, theorical_evolution=theorical_evolution, chart_data=projection_to_template)
-
     return render_template('index.html', products=products_data, electricity_price=0.16, theorical_evolution=0.008236)
 
 if __name__ == '__main__':
